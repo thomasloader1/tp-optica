0283782b9d621158f622874198e266373d48a318
Sergio gentiletti <gentiletti.sergio@gmail.com>
2019-08-14 21:42:13 -0300

    Agregando cosas rarass... MDB

--- src/UI/layout/Footer.js
+++ src/UI/layout/Footer.js
@@ -1,13 +1,11 @@
 import React, { Component, Fragment } from 'react'
+import FooterPage from "../../aux-components/FooterPage";
 
 export default class Footer extends Component {
     render() {
         return (
             <Fragment>
-                <footer className="navbar fixed-bottom navbar-light bg-light">
-                     <a className="navbar-brand" href="#">Footer</a> 
-                     <p>&copy; Copyright - 2019</p>
-                </footer>
+               <FooterPage />
             </Fragment>
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,23 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './UI/layout/App';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import store from './api/store'
 
 ReactDOM.render(
-<Provider store = {store}>
-<Router>
-<App/>
-
-</Router>
-
-</Provider>
-            
-      ,
+  <Provider store = {store}>
+    <Router>
+      <App/>
+    </Router>
+  </Provider>,
     document.getElementById('root')
 );
