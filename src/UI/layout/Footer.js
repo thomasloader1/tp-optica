9c0eb628d5ec6a01f16c0a0dada5ff78ccd5b7c0
Tomas <talk.tomas@hotmail.com>
2019-08-15 09:33:29 -0300

    Back to old school

--- src/UI/layout/Footer.js
+++ src/UI/layout/Footer.js
@@ -1,11 +1,10 @@
 import React, { Component, Fragment } from 'react'
-import FooterPage from "../../aux-components/FooterPage";
 
 export default class Footer extends Component {
     render() {
         return (
             <Fragment>
-               <FooterPage />
+              <p>asd</p>
             </Fragment>
         )
     }
--- src/UI/layout/Header.js
+++ src/UI/layout/Header.js
@@ -1,14 +1,31 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
-import NavbarPage from '../../aux-components/NavbarPage'
 
 class Header extends Component {
     render() {
         let { links } = this.props
         return (
             <Fragment>
-                <NavbarPage/>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                </ul>
+            </div>
+            </nav>
             </Fragment>
         )
     }
